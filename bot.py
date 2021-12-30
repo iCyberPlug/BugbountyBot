74adc84fb96649bd79c54ba1d9b98bf49615e125
Erik451 <47476901+erik-451@users.noreply.github.com>
2021-12-30 02:41:05 +0100

    Update v0.1
    English Traduction

--- bot.py
+++ bot.py
@@ -7,21 +7,22 @@ import datetime
 import requests
 
 ##################
-###  COMANDOS  ###
+###  COMMANDS  ###
 ##################
 
-# Caracter por el cual identifica si se dirige al bot
-bot = commands.Bot(command_prefix='$', description="Bot de auditoria web")
+# Character by which it identifies if it goes to the bot
+# Description bot
+bot = commands.Bot(command_prefix='$', description="Web audit bot"")
 
-### 1- Muestra todos los comandos disponibles ###
+### 1- Show all available commands ###
 @bot.command()
 async def ayuda(ctx):
-    await ctx.send("```php\n Esto es lo que buscas: \n $ayuda Muestra los comandos permitidos. \n $info Informacion del servidor. \n $ping Testeo de respuesta. \n $sub Introduce una pagina y te muestra sus subdominios. Ejemplo: $sub discord.com \n $take Indica si existe un subdominio vulnerable al SubdomainTakeOver. Ejemplo: $take discord.com \n $dir Codigo de respuesta del directorio indicado de todos los subdominios. Ejemplo: $dir discord.com /robots.txt \n $bitcoin Precio del bitcoin actual.```")
+    await ctx.send("```bash\n Results: \n $show Show allowed commands. \n $info Server information. \n $sub Enter a page and show you its subdomains. Example: $sub discord.com \n $take Indicates whether there is a subdomain vulnerable to SubdomainTakeOver. Example: $take discord.com \n $dir Status code of the indicated directory of all subdomains. Example: $dir discord.com /robots.txt \n $bitcoin Current bitcoin price.```")
 
-### 2- Muestra infomacion sobre el discord ###
+### 2- Displays information about the discord ###
 @bot.command()
 async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Informacion sobre el servidor", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Information about the server", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
     embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
     embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
@@ -30,39 +31,34 @@ async def info(ctx):
     #embed.set_thumbnail(url="<url_icon>")
     await ctx.send(embed=embed)
 
-### 3- Test de respuesta al poner $ping ##
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-### 4- Busca subdominios ###
+### 3- Looking for subdomains ###
 @bot.command()
 async def sub(ctx, domain):
-    await ctx.send("Escaneando subdominios...")
+    await ctx.send("Scanning subdomains...")
     os.system("findomain -t" +  domain + "> /tmp/subs.txt")
-    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>' > /tmp/subdominios.txt")
-    await ctx.send(file=discord.File(r'/tmp/subdominios.txt'))
+    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>' > /tmp/subdomains.txt")
+    await ctx.send(file=discord.File(r'/tmp/subdomains.txt'))
 
-### 5- Subdomain Takeover ###
+### 4- Subdomain Takeover ###
 @bot.command()
 async def take(ctx, take):
-    await ctx.send("Buscando SubdomainTakeOver...")
-    os.system("findomain -t" +  take + "> /tmp/subdominios.txt")
-    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>'|httprobe --prefer-https > /tmp/subdominios.txt")
-    os.system("subzy --targets /tmp/subdominios.txt" + "> /tmp/take.txt")
+    await ctx.send("Looking for SubdomainTakeOver...")
+    os.system("findomain -t" +  take + "> /tmp/subs.txt"")
+    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>'|httprobe --prefer-https > /tmp/subdomains.txt")
+    os.system("subzy --targets /tmp/subdomains.txt" + "> /tmp/take.txt")
     os.system("cat /tmp/take.txt|egrep -v 'Loaded'|egrep -v 'default'|egrep -v 'Concurrent'|egrep -v 'verify_ssl'|egrep -v 'timeout'|egrep -v 'hide_fails'|sed 's|[]\[]0m| |g'|sed 's|[]\[]31m| |g' > /tmp/subtakeover.txt")
     await ctx.send(file=discord.File(r'/tmp/subtakeover.txt'))
 
-### 6- Rutas existentes ###
+### 5- See Directories Status ###
 @bot.command()
 async def dir(ctx, domain, rute):
-    await ctx.send("Escaneando directorios...")
+    await ctx.send("Scanning directories...")
     os.system("findomain -t" +  domain + "> /tmp/subs.txt")
-    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>'|httprobe --prefer-https > /tmp/subdominios.txt")
-    os.system("subcheck /tmp/subdominios.txt " + rute + "> /tmp/directorios.txt")
-    await ctx.send(file=discord.File(r'/tmp/directorios.txt'))
+    os.system("cat /tmp/subs.txt|egrep -v 'Searching'|egrep -v 'Job'|egrep -v 'Good'|egrep -v 'Target ==>'|httprobe --prefer-https > /tmp/subdominains.txt")
+    os.system("subcheck /tmp/subdomains.txt " + rute + "> /tmp/directories.txt")
+    await ctx.send(file=discord.File(r'/tmp/directories.txt'))
 
-### 7- Muestra el precio del bitcoin ###
+### 6- Shows the price of bitcoin ###
 @bot.command()
 async def bitcoin(ctx):
     url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
@@ -70,13 +66,13 @@ async def bitcoin(ctx):
     value = response.json()['bpi']['USD']['rate']
     await ctx.send("```json\n Bitcoin price is: " + value + " USD```")
 
-################################
-### Inicio de sesion del bot ###
-################################
-# Que esta haciendo el bot
+#################
+### Bot Login ###
+#################
+# Bot activities
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
-    await bot.change_presence(activity=discord.Game(name="Auditando Webs"))
+    await bot.change_presence(activity=discord.Game(name="Auditing Websites"))
 
 bot.run('<BOT_TOKEN>')
